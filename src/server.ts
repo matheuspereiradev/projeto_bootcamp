8da5415672426174613f13f268140eac46cc977e
matheus <matheuslima20111997@gmail.com>
2020-11-01 22:49:18 -0300

    start point

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3000)
+app.get('/',(req,res)=>{
+    res.json({
+        mensagem:"teste"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('rodando')
+})
 
 //npm install -D typescript
 //npx tsc --init
+
+//npm install -D ts-node-dev       para nao ter o dist
