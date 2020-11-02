36df79d62a5758d79c2e610960df776ccc7b902f
matheus <matheuslima20111997@gmail.com>
2020-11-02 11:20:43 -0300

    config

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.json({
-        mensagem:"teste"
-    })
-})
+app.get('/', (req, res) => {
+  res.json({
+    mensagem: 'teste',
+  });
+});
 
-app.listen(3000,()=>{
-    console.log('rodando')
-})
+app.listen(3000, () => {
+  console.log('rodando');
+});
 
-//npm install -D typescript
-//npx tsc --init
+//  npm install -D typescript
+//  npx tsc --init
 
-//npm install -D ts-node-dev       para nao ter o dist
+//  npm install -D ts-node-dev       para nao ter o dist
